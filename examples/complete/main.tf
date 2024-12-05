0927d9fe0f8289c84304e7e124acb96ba6a51b3a
shanye <shanye.xzq@alibaba-inc.com>
2024-12-05 16:14:35 +0800

    examples: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,6 +1,3 @@
-variable "name" {
-  default = "terraform-example"
-}
 
 data "alicloud_adb_zones" "default" {}
 
@@ -8,16 +5,16 @@ data "alicloud_vpcs" "default" {
   name_regex = "^default-NODELETING$"
 }
 data "alicloud_vswitches" "default" {
-  vpc_id  = data.alicloud_vpcs.default.ids.0
-  zone_id = data.alicloud_adb_zones.default.ids.0
+  vpc_id  = data.alicloud_vpcs.default.ids[0]
+  zone_id = data.alicloud_adb_zones.default.ids[0]
 }
 
 module "adb_example" {
   source = "../.."
 
-  vswitch_id          = data.alicloud_vswitches.default.vswitches.0.id
-  availability_zone   = data.alicloud_vswitches.default.vswitches.0.zone_id
-  db_cluster_version  = "3.0"
+  vswitch_id          = data.alicloud_vswitches.default.vswitches[0].id
+  availability_zone   = data.alicloud_vswitches.default.vswitches[0].zone_id
+  db_cluster_version  = null
   db_cluster_category = "Cluster"
   db_node_class       = "C8"
   db_node_count       = 1
